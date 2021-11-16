3aaf71ffea4464d3dab8f634423ed8f724cfd65d
hwangJi-dev <melon2228@swu.ac.kr>
2021-11-16 14:06:29 +0900

    ⚰️[DEL] : ChatVC 쓸모없는 코드 삭제

--- SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Screen/Chat/Controller/ChatVC.swift
+++ SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Screen/Chat/Controller/ChatVC.swift
@@ -24,6 +24,4 @@ class ChatVC: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
