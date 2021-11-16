6408c45285aa25d893d021684313186a402e2e7b
hwangJi-dev <melon2228@swu.ac.kr>
2021-11-17 04:21:25 +0900

    ➕[ADD] : chatGray 컬러셋 추가

--- SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Global/Extension/UIColor+Additions.swift
+++ SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Global/Extension/UIColor+Additions.swift
@@ -8,41 +8,45 @@
 import UIKit
 
 extension UIColor {
-
-  @nonobjc class var main: UIColor {
-    return UIColor(red: 61.0 / 255.0, green: 150.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var white: UIColor {
-    return UIColor(red: 254.0 / 255.0, green: 1.0, blue: 1.0, alpha: 1.0)
-  }
-
-  @nonobjc class var gray1: UIColor {
-    return UIColor(white: 245.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var gray2: UIColor {
-    return UIColor(white: 188.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var black: UIColor {
-    return UIColor(white: 0.0, alpha: 1.0)
-  }
-
-  @nonobjc class var gray5: UIColor {
-    return UIColor(white: 74.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var copySub2: UIColor {
-    return UIColor(red: 150.0 / 255.0, green: 149.0 / 255.0, blue: 147.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var gray6: UIColor {
-    return UIColor(white: 250.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var gray3: UIColor {
-    return UIColor(red: 138.0 / 255.0, green: 138.0 / 255.0, blue: 140.0 / 255.0, alpha: 1.0)
-  }
-
+    
+    @nonobjc class var main: UIColor {
+        return UIColor(red: 61.0 / 255.0, green: 150.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var white: UIColor {
+        return UIColor(red: 254.0 / 255.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray1: UIColor {
+        return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray2: UIColor {
+        return UIColor(white: 188.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var black: UIColor {
+        return UIColor(white: 0.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray5: UIColor {
+        return UIColor(white: 74.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var copySub2: UIColor {
+        return UIColor(red: 150.0 / 255.0, green: 149.0 / 255.0, blue: 147.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray6: UIColor {
+        return UIColor(white: 250.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray3: UIColor {
+        return UIColor(red: 138.0 / 255.0, green: 138.0 / 255.0, blue: 140.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var chatGray: UIColor {
+        return UIColor(red: 243.0 / 255.0, green: 243.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+    }
+    
 }
