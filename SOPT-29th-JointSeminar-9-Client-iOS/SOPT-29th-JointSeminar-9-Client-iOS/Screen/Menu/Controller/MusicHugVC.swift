082c348804801af3f89f609b8c924493a87f7ab8
임주민 <limj1213@gmail.com>
2021-11-18 11:42:00 +0900

    ✅[CHORE] : 상단 네비게이션 탭바 레이아웃 추가

--- SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Screen/Menu/Controller/MusicHugVC.swift
+++ SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Screen/Menu/Controller/MusicHugVC.swift
@@ -21,31 +21,41 @@ class MusicHugVC: UIViewController {
     //MARK: Navigation Tabbar
     func setNC() {
         
+        //MARK: 왼쪽 버튼
         let backButton = UIButton().then {
             $0.setBackgroundImage(UIImage(named: "main_ic_back"), for: .normal)
             $0.addTarget(self, action: #selector(back), for: .touchUpInside)
             $0.tintColor = .black
         }
         let leftItem = UIBarButtonItem(customView: backButton)
-        self.navigationItem.leftBarButtonItem = leftItem
+        let leftSpacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace,
+                                         target: nil, action: nil).then {
+            $0.width = 21
+        }
+        navigationItem.setLeftBarButtonItems([leftSpacer, leftItem], animated: false)
         
+        //MARK: 가운데 타이틀 라벨
         let titleLabel = UILabel().then {
-          $0.textAlignment = .center
-          $0.font = UIFont.AppleSDGothicSB(size: 19)
+            $0.textAlignment = .center
+            $0.font = UIFont.AppleSDGothicSB(size: 19)
             $0.text = "뮤직허그"
         }
-        self.navigationItem.titleView = titleLabel
+        navigationItem.titleView = titleLabel
         
-        
+        //MARK: 오른쪽 버튼
         let profileButton = UIButton().then {
             $0.setBackgroundImage(UIImage(named: "main_img_profile"), for: .normal)
         }
         let rightItem = UIBarButtonItem(customView: profileButton)
-        self.navigationItem.rightBarButtonItem = rightItem
+        let rightSpacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace,
+                                          target: nil, action: nil).then {
+            $0.width = 19
+        }
+        navigationItem.setRightBarButtonItems([rightSpacer, rightItem], animated: false)
     }
     
     //MARK: - @objc Function Part
     @objc func back() {
-        self.navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
     }
 }
