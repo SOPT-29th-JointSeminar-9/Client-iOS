89e33b24979d1e0e057d5f94291c598eda8dc427
hwangJi-dev <melon2228@swu.ac.kr>
2021-11-19 17:41:52 +0900

    ✅[CHORE] : String Extension Func 추가

--- SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Global/Extension/String+.swift
+++ SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Global/Extension/String+.swift
@@ -21,6 +21,17 @@ extension String {
         return estimatedFrame
     }
     
+    func heightForLabel(text:String, font:UIFont, width:CGFloat) -> CGFloat {
+            let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+            label.numberOfLines = 0
+            label.lineBreakMode = NSLineBreakMode.byWordWrapping
+            label.font = font
+            label.text = text
+
+            label.sizeToFit()
+            return label.frame.height
+        }
+    
     func height(constraintedWidth width: CGFloat, font: UIFont) -> CGFloat {
         let label =  UILabel(frame: CGRect(x: 0, y: 0, width: width, height: .greatestFiniteMagnitude))
         label.numberOfLines = 0
