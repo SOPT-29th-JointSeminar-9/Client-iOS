09b40f61fa48902a541545ca6c7703deac3989f9
hwangJi-dev <melon2228@swu.ac.kr>
2021-11-19 17:41:30 +0900

    ➕[ADD] : BasePaddingLabel Class 추가

---
+++ SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Global/Class/BasePaddingLabel.swift
@@ -0,0 +1,49 @@
+//
+//  BasePaddingLabel.swift
+//  SOPT-29th-JointSeminar-9-Client-iOS
+//
+//  Created by 황지은 on 2021/11/19.
+//
+
+import UIKit
+@IBDesignable
+class BasePaddingLabel: UILabel {
+    var textEdgeInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    open override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = bounds.inset(by: textEdgeInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textEdgeInsets.top, left: -textEdgeInsets.left, bottom: -textEdgeInsets.bottom, right: -textEdgeInsets.right)
+        return textRect.inset(by: invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: textEdgeInsets))
+    }
+    
+    @IBInspectable
+    var paddingLeft: CGFloat {
+        set { textEdgeInsets.left = newValue }
+        get { return textEdgeInsets.left }
+    }
+    
+    @IBInspectable
+    var paddingRight: CGFloat {
+        set { textEdgeInsets.right = newValue }
+        get { return textEdgeInsets.right }
+    }
+    
+    @IBInspectable
+    var paddingTop: CGFloat {
+        set { textEdgeInsets.top = newValue }
+        get { return textEdgeInsets.top }
+    }
+    
+    @IBInspectable
+    var paddingBottom: CGFloat {
+        set { textEdgeInsets.bottom = newValue }
+        get { return textEdgeInsets.bottom }
+    }
+}
