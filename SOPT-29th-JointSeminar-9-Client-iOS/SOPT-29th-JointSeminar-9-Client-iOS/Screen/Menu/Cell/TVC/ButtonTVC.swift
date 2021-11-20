fb7a331c956a00605a32b3779ba425d1f23aed2b
임주민 <limj1213@gmail.com>
2021-11-20 15:23:24 +0900

    ⚡️[IMPROVE] : 개설하기 버튼 수정

--- SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Screen/Menu/Cell/TVC/ButtonTVC.swift
+++ SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Screen/Menu/Cell/TVC/ButtonTVC.swift
@@ -11,11 +11,19 @@ class ButtonTVC: UITableViewCell {
     
     @IBOutlet weak var createButton: UIButton!
     
+    var buttonTapped:((ButtonTVC?) -> Void)? = nil
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    @IBAction func createBtnDidTap(_ sender: UIButton) {
+        if let btnAction = self.buttonTapped {
+            btnAction(self)
+        }
     }
 }
