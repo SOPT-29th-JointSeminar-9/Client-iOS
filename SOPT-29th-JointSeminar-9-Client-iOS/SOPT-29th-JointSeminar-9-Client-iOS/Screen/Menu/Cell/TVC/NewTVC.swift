e91ab7dcd8c256913689dbb96c6b1b315a9de913
jumining <limj1213@gmail.com>
2021-11-25 18:52:27 +0900

    ➕[ADD] : NewTVC파일과 xib파일 추가

---
+++ SOPT-29th-JointSeminar-9-Client-iOS/SOPT-29th-JointSeminar-9-Client-iOS/Screen/Menu/Cell/TVC/NewTVC.swift
@@ -0,0 +1,19 @@
+//
+//  NewTVC.swift
+//  SOPT-29th-JointSeminar-9-Client-iOS
+//
+//  Created by 임주민 on 2021/11/25.
+//
+
+import UIKit
+
+class NewTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
